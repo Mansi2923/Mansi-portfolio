912ad530f0f061a80c9ef1db2f50e679efccd6d5
Mansi2923 <sawantmanasi239@gmail.com>
2025-06-15 13:08:04 -0400

    Add Vercel Speed Insights

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 import './App.css';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function App() {
   return (
@@ -36,6 +37,7 @@ function App() {
       </main>
       <Footer />
       <Analytics />
+      <SpeedInsights />
     </div>
   );
 }
